2e557d3c7d31d9f815cf0ee3bd84909866cc4a28
Diego Borrajo <dieborr3@gmail.com>
2024-10-26 14:24:19 +0200

    Add pwm with fixes
    Se ha añadido en el main la lógica para poder mover los motores en todas las direcciones, además se ha cambiado la estructura para cumplir requisito de estilo de código

---
+++ PWM/Objects/pwm.d
@@ -0,0 +1,10 @@
+.\objects\pwm.o: pwm.c
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.2\Device\Include\LPC17xx.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\objects\pwm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\pwm.o: C:\Users\dieborr\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.2\Device\Include\system_LPC17xx.h
+.\objects\pwm.o: reg_masks.h
