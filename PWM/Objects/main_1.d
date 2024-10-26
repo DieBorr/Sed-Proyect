cc8a48ae26ed8a9f861249a2cdce4e4a8029b509
Diego Borrajo <dieborr3@gmail.com>
2024-10-26 14:24:19 +0200

    Archivo previo a la entrega
    Se ha añadido en el main la lógica para poder mover los motores en todas las direcciones, además se ha cambiado la estructura para cumplir requisito de estilo de código

--- PWM/Objects/main_1.d
+++ PWM/Objects/main_1.d
@@ -7,4 +7,4 @@
 .\objects\main_1.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\main_1.o: C:\Users\dieborr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
 .\objects\main_1.o: C:\Users\dieborr\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.2\Device\Include\system_LPC17xx.h
-.\objects\main_1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\main_1.o: reg_masks.h
